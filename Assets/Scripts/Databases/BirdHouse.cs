662df1c9d5518caa4129ff769f20ed56fbf0c835
vedderjam <vedderjam@gmail.com>
2020-07-23 13:36:52 +0200

    Manage the database of birds

---
+++ Assets/Scripts/Databases/BirdHouse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class BirdInfo
+{
+    public string name;
+    public Sprite sprite;
+    public int prize;
+    public bool purchased;
+    public GameObject prefab;
+}
+
+[CreateAssetMenu]
+public class BirdHouse : ScriptableObject
+{
+    public List<BirdInfo> birdInfos;
+
+    public bool Purchase(int index, ref int coins)
+    {
+        if(index < 0 || index >= birdInfos.Capacity)
+        {
+            return false;
+        }
+        else if(coins < birdInfos[index].prize)
+        {
+            return false;
+        }
+        else
+        {
+            if (birdInfos[index].purchased)
+            {
+                return false;
+            }
+            else
+            {
+                coins -= birdInfos[index].prize;
+                birdInfos[index].purchased = true;
+                return true;
+            }
+        }
+    }
+}
