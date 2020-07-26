f49f246578dd61628928416332fc35b29dfbfbf9
vedderjam <vedderjam@gmail.com>
2020-07-26 18:15:45 +0200

    Animated logo

---
+++ Assets/Scripts/LogoAnimation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoAnimation : MonoBehaviour
+{
+    public void OnAnimationEnded()
+    {
+        gameObject.SetActive(false);
+    }
+}
