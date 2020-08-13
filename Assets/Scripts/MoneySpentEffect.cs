593d5e0218aed14aa19f764f432278e2f5d783d8
vedderjam <vedderjam@gmail.com>
2020-08-13 19:47:05 +0200

    Removed unnecessary event

--- Assets/Scripts/MoneySpentEffect.cs
+++ Assets/Scripts/MoneySpentEffect.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class MoneySpentEffect : MonoBehaviour
 {
-    public void OnEffectStarted()
-    {
-        Debug.Log("Effect started");
-    }
-
     public void OnEffectFinished()
     {
         gameObject.SetActive(false);
-        Debug.Log("Effect finished");
     }
 }
